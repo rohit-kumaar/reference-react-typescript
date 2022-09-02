72aab4aac562b5d31ff9d9a130515e5e0598ea73
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-09-02 14:52:14 +0530

    Event Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import Button from "./components/Button";
 import Greet from "./components/Greet";
 import Heading from "./components/Heading";
+import Input from "./components/Input";
 import Oscar from "./components/Oscar";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
@@ -24,12 +26,19 @@ function App() {
       <Person name={personName} />
       <PersonList names={nameList} /> */}
 
-      <Status status="loading" />
+      {/* <Status status="loading" />
       <Heading>Placeholder text</Heading>
       <Oscar>
         <Heading>Oscar goes to Leonardo Dicaprio!</Heading>
       </Oscar>
-      <Greet name="Rohit" isLoggedIn={true} />
+      <Greet name="Rohit" isLoggedIn={true} /> */}
+
+      <Button
+        handleClick={(event, id) => {
+          console.log("Button clicked", event, id);
+        }}
+      />
+      <Input value="" handleChange={(event) => console.log(event)} />
     </div>
   );
 }
