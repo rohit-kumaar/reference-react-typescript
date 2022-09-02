4c8c8c2a6d44df7ea98f13105fd1a7657ae89758
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-09-02 12:15:31 +0530

    Typing props

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Greet from "./components/Greet";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Greet name="Rohit" />
+    </div>
+  );
 }
 
 export default App;
