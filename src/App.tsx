2e1c153f27f67769f205613aaa77b86831ac3580
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-09-03 13:16:31 +0530

    useState Hook

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Input from "./components/Input";
 import Oscar from "./components/Oscar";
 import Person from "./components/Person";
 import PersonList from "./components/PersonList";
+import LoggedIn from "./components/state/LoggedIn";
 import Status from "./components/Status";
 
 function App() {
@@ -40,9 +41,11 @@ function App() {
       />
       <Input value="" handleChange={(event) => console.log(event)} /> */}
 
-      <Input value="" handleChange={(event) => console.log(event)} />
+      {/* <Input value="" handleChange={(event) => console.log(event)} />
       <Person name={personName} />
-      <PersonList names={nameList} />
+      <PersonList names={nameList} /> */}
+
+      <LoggedIn />
     </div>
   );
 }
